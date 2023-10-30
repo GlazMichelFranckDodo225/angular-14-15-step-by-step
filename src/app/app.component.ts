497f2b85440da9157b3384c86e238f6a055b74f2
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2023-10-30 06:13:29 +0100

    Project Initialization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-14-15-step-by-step';
+
+export class AppComponent implements OnInit{
+  title: string;
+  firstName: string;
+  lastName: string;
+
+  constructor() {
+    this.title = 'angular 14/15 Step by Step';
+    this.firstName = 'Glaz';
+    this.lastName = 'Dodo';
+  }
+
+  ngOnInit(): void {
+      
+  }
 }
